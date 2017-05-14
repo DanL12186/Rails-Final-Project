ffe8fd4dab925be48591dd4f560d53b710ffc7f2
DanL12186 <danlrcn@hotmail.com>
2017-05-14 14:15:24 -0400

    added a few generic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, uniqueness: true, presence: true #validates_presence_of
+  validates :password_digest, presence: true, length: { minimum: 8 } #do I need presence if I have length?
+
 end
