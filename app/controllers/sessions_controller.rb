5d7c7d7483ef54b71111dd4848a05a349d442211
DanL12186 <danlrcn@hotmail.com>
2017-05-14 15:43:13 -0400

    added some basic (probably temporary) session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
 
+  def create
+    @user = User.find_by(name: session_params[:name])
+    session[:user_id] = @user.id  if  @user && @user.authenticate(session_params[:password])
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:password, :name)
+  end
+
 end
