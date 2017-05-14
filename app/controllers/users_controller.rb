ca13bd7decdbdc1ff2ddafc8e3ef55d40821bb3e
DanL12186 <danlrcn@hotmail.com>
2017-05-14 15:49:26 -0400

    added boilerplate 'create' function for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id #does it make sense to set this in both the sessions and user controller? 
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
