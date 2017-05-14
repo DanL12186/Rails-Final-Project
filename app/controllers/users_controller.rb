5aa3e9f258bf98ce0b38e4647bc168981d645e12
DanL12186 <danlrcn@hotmail.com>
2017-05-14 14:19:29 -0400

    added user_params, and included basic set_user private function for before_action method on edit/show; @users for index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user!, only: [:show, :edit]
 
   def new
     @user = User.new
@@ -10,13 +11,25 @@ class UsersController < ApplicationController
   def show
   end
 
-  def edit
+  def edit #don't really need
   end
 
   def update
   end
 
   def index
+    @users = User.all
   end
+
+  private
+
+  def set_user!
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, uniqueness: true, presence: true #validates_presence_of
+  validates :name, uniqueness: true, presence: true #validates_presence_of format instead?
   validates :password_digest, presence: true, length: { minimum: 8 } #do I need presence if I have length?
 
 end
