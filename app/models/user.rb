54b32df529ec8b58b280f87e78819c1bb653e5b6
DanL12186 <danlrcn@hotmail.com>
2017-05-16 11:44:28 -0400

    added temporary log_out button, added logout root, fixed password check..
    Password check which I had mistakenly set to check the length of the
    password digest rather than the actual password entered (which, once
    salted, will always be >4 chars anyway). Added conditional to email
    uniqueness check, which would otherwise serve as a presence check as
    well.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, uniqueness: true, presence: true #validates_presence_of format instead?
-  validates :email, uniqueness: true
-  validates :password_digest, presence: true, length: { minimum: 4 } #do I need presence if I have length?
+  validates :email, uniqueness: true, if: 'email.present?'
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 4 } #do I need presence if I have length?
   validates_confirmation_of :password
 
 end
