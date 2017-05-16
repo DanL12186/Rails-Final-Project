33d4df8aaaae668a8617317ece58701d04037ecd
DanL12186 <danlrcn@hotmail.com>
2017-05-16 10:35:17 -0400

    Created /application/index, added some code to test...
    …Whether or not things are working. Temporarily changed password length
    requirement length to four characters to ease testing. Added email
    uniqueness validation but not presence validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, uniqueness: true, presence: true #validates_presence_of format instead?
-  validates :password_digest, presence: true, length: { minimum: 8 } #do I need presence if I have length?
+  validates :email, uniqueness: true
+  validates :password_digest, presence: true, length: { minimum: 4 } #do I need presence if I have length?
   validates_confirmation_of :password
 
 end
