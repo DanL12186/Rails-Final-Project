a8f9b9ce733f5b8d517c3c22d7825ea19803437b
DanL12186 <danlrcn@hotmail.com>
2017-05-17 16:42:43 -0400

    removed password_digest validation because has_secure_password..
    already checks for this and password. Moved uniqueness to the bottom so
    if the others fail the database won’t be queried.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: true, allow_blank: true
   validates :password, length: { minimum: 4 }
-  validates :password_digest, presence: true
   validates_confirmation_of :password
+  validates :name, uniqueness: { case_sensitive: false }
 
 end
