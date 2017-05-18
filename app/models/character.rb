0126cb8c8855e2cbfa20bceade04420c476c0410
DanL12186 <danlrcn@hotmail.com>
2017-05-17 20:08:57 -0400

    filled out character/show model relationships, added to user's

---
+++ app/models/character.rb
@@ -0,0 +1,4 @@
+class Character < ApplicationRecord
+  belongs_to :show
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :shows
+  has_many :characters, through: :shows
+
   validates :email, uniqueness: true, allow_blank: true
   validates :password, length: { minimum: 4 }
   validates_confirmation_of :password
