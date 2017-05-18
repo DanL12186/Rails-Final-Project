f38fd802cf651cf1d8be038ce83f24c729ba7501
DanL12186 <danlrcn@hotmail.com>
2017-05-17 20:09:41 -0400

    Created a very basic outline of ShowsController

---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,30 @@
+class ShowsController < ApplicationController
+  before_action :set_show, only: [:edit, :show, :update]
+
+  def new
+    @show = Show.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_show
+    @show = Show.find(params[:id])
+  end
+
+  def show_params
+    params.require(:show).permit(:name, :user_id)
+  end
+
+end
