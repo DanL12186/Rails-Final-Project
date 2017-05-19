31e3982756c1f079b81c59f9f3add4945ac84095
DanL12186 <danlrcn@hotmail.com>
2017-05-18 21:55:01 -0400

    added accepted_nested_attributes for future use

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,8 @@ class Show < ApplicationRecord
   belongs_to :user
   has_many :characters
 
+  accepts_nested_attributes_for :characters
+
   validates :name, presence: true
   validates :user_id, presence: true
 end
