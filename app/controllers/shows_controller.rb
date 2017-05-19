644777c04c1f7e8f4b47807d4b24fce1ee4f0ee1
DanL12186 <danlrcn@hotmail.com>
2017-05-19 15:19:24 -0400

    Added some basic actions to shows_controller...
    to try to get form for creating new show with multiple characters to
    work. So far, very little success.

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,11 +1,14 @@
 class ShowsController < ApplicationController
   before_action :set_show, only: [:edit, :show, :update]
+  # before_action :deny_unauthorized_access, only: [:new, :edit, :update, :destroy]
 
   def new
     @show = Show.new
+    @show.characters.build() #x.times do
   end
 
   def create
+    @show = Show.create(show_params)
   end
 
   def edit
@@ -24,11 +27,11 @@ class ShowsController < ApplicationController
   private
 
   def set_show
-    @show = Show.find(params[:id])
+    @show = Show.find(show_params[:user_id])
   end
 
   def show_params
-    params.require(:show).permit(:name, :user_id)
+    params.require(:show).permit(:name, :user_id, characters_attributes: [:show_id, :dislike, :deceased, :name])
   end
 
 end
