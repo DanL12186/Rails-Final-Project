9e7a9ef69d4e3b7e97633e293af4272452509dac
DanL12186 <danlrcn@hotmail.com>
2017-05-22 13:14:37 -0400

    Added some helpful links to the home page...
    Fixed an issue (by replacing an object with a string) where an entire
    ActiveRecord CollectionProxy object was showing up in the URL from
    certain links when no actual data was needed. Got rid of unused User’s
    index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
-  def index
-  end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
