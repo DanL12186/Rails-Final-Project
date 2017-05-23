364ff364df5f3061bc4ba27e19a4f1d14a211ddd
DanL12186 <danlrcn@hotmail.com>
2017-05-23 10:31:26 -0400

    Added a sort_by_name_length method. May use this later.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,9 @@ module ApplicationHelper
     self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
     ((count.to_f / self.characters.size) * 100) + 50
   end
-    
+
+  def sort_by_name_length
+    self.sort_by { |item| item.name.length }
+  end
+
 end
