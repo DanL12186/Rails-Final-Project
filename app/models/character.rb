8632c73a991bc4ede1ca3fbc272c5ba5ee768493
DanL12186 <danlrcn@hotmail.com>
2017-05-22 20:43:57 -0400

    Added validation to ensure a user can't have...
    two characters of the same name in the same show. Added two scope
    methods to allow selecting for living characters the user likes. Put a
    primitive implementation using those two scopes in character index.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,18 @@
 class Character < ApplicationRecord
   include CharactersHelper
 
+  scope :like, -> { where(dislike: false) }
+  scope :alive, -> { where(deceased: false) }
+
   belongs_to :show
 
   validates :name, presence: true
-  
+
+  validate :user_shows_have_unique_characters
+
+  def user_shows_have_unique_characters
+    binding.pry
+    errors.add(:show, "Cannot have two characters with the same name in one show") if self.show.characters.any? {|char| char.name == self.name }
+  end
+
 end
