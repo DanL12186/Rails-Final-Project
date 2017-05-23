e177f4331ad7d861e1ed39dffe087e0d191e38e5
DanL12186 <danlrcn@hotmail.com>
2017-05-23 11:27:19 -0400

    Moved some rounding logic from view to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def happiness
     count = 0
     self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
-    ((count.to_f / self.characters.size) * 100) + 50
+    ((count.to_f / self.characters.size * 100) + 50).round(1)
   end
 
   def sort_by_name_length
