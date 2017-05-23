12fadeebe4c9862e3ac617d2aa35eaead1758c15
DanL12186 <danlrcn@hotmail.com>
2017-05-22 22:06:07 -0400

    Shows have unique characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,16 +7,10 @@ class Character < ApplicationRecord
   belongs_to :show
 
   validates :name, presence: true
-  # validates :name, uniqueness: { scope: :show }
-  # validate :user_shows_have_unique_characters
-  #
-  # def user_shows_have_unique_characters
-  #   errors.add(:show, "Cannot have two characters with the same name in one show") if self.show.characters.any? {|char| char.name == self.name }
-  # end
+  validates :name, uniqueness: { scope: :show }
 
   def show_attributes=(show)
     self.show = Show.find_or_create_by(name: show.name)
     self.show.update(show)
   end
-
 end
