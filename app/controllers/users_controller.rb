e83d70689d9d6c030cb3a774caecc623bc41c3d1
DanL12186 <danlrcn@hotmail.com>
2017-05-24 15:15:09 -0400

    Characters and New Users (user signup) now post errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save ? (session[:user_id] = @user.id; redirect_to user_path(@user)) : (redirect_to new_user_path)
+    @user.save ? (session[:user_id] = @user.id; redirect_to user_path(@user)) : (render :new)
   end
 
   def edit
