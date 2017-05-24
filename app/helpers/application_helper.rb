c57c2f1904658075c3c819294825970302a1e422
DanL12186 <danlrcn@hotmail.com>
2017-05-24 12:59:10 -0400

    restructured #happiness function. Modified sort_by_name_length...
    to be flexible by accepting a variable (no longer works with just shows)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
 
   def happiness
-    count = 0
     unless self.characters.empty?
+      count = 0
       self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
-      ((count / self.characters.size * 100) + 50).round(1)
     end
+    ((count / self.characters.size * 100) + 50).round(1)
   end
 
   def happiest
     @shows.select {|show| !show.characters.empty?}.max_by { |show| show.happiness }
   end
 
-  def sort_by_name_length
-    @shows.sort_by { |show| show.name.length }
+  def sort_by_name_length(index)
+    index.sort_by { |show| show.name.length }
   end
 
 end
