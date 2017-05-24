c1661341a6ff81cc9f11eabb9e76b91d1348b29f
DanL12186 <danlrcn@hotmail.com>
2017-05-24 15:22:12 -0400

    Added in min name length. Uniqueness doesn't prevent creation..

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, allow_blank: true
   validates :password, length: { minimum: 4 }
   validates_confirmation_of :password
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, length: {minimum: 3}, uniqueness: { case_sensitive: false }
 
   def self.find_or_create_by_omniauth(auth_hash)
     where(email: auth_hash[:info][:email]).first_or_create do |user|
