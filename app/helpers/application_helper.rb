8c6dc97ca1ebc4bba5a8b42ab376a642021e6ad0
DanL12186 <danlrcn@hotmail.com>
2017-05-24 12:50:53 -0400

    Added some additional, fairly ugly logic to #happiness.....
    and #happiest methods in order to ensure that empty shows can be
    created and will not cause the page to crash while attempting to
    compare nil to float or divide by zero.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,15 @@ module ApplicationHelper
 
   def happiness
     count = 0
-    self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
-    ((count.to_f / self.characters.size * 100) + 50).round(1)
+    unless self.characters.empty?
+      self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
+      ((count / self.characters.size * 100) + 50).round(1)
+    end
   end
 
   def happiest
-    @shows.max_by { |show| show.happiness }
+    shows_with_characters = @shows.select {|show| !show.characters.empty?}
+    shows_with_characters.max_by { |show| show.happiness }
   end
 
   def sort_by_name_length
