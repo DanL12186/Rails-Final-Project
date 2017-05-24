e0a0ea6058b0bb190cdab74b7d1bf01cf49f6cd2
DanL12186 <danlrcn@hotmail.com>
2017-05-24 16:41:42 -0400

    "de-factored" code and added simple flash error message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,13 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: session_params[:name])
     session[:user_id] = @user.id  if  @user && @user.authenticate(session_params[:password])
-    !!session[:user_id] ? (redirect_to user_path(@user)) : (redirect_to login_path)
+    if !!session[:user_id]
+      redirect_to user_path(@user)
+    else
+      @user = User.new(name: session_params[:name])
+      flash[:error] = "Username and password don't match"
+      render :new
+    end
   end
 
   def destroy
