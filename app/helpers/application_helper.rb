16106e54a5bbf4318e383b55d468a3c2d6f2cc09
DanL12186 <danlrcn@hotmail.com>
2017-05-24 17:18:47 -0400

    Fixed yet another error for shows with no characters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
 
   def happiness
-    unless self.characters.empty?
-      count = 0
-      self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
-    end
+    count = 0
+    return nil if self.characters.empty?
+    self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
     ((count / self.characters.size * 100) + 50).round(1)
   end
 
