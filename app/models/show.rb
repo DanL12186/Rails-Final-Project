f17e6ae5dd42fd306f3c7946a655ad38e3562647
DanL12186 <danlrcn@hotmail.com>
2017-05-25 01:05:29 -0400

    Replaced accepts_nested_attributes with an uninspired custom writer

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,15 +3,20 @@ class Show < ApplicationRecord
   belongs_to :user
   has_many :characters, dependent: :destroy
 
-  accepts_nested_attributes_for :characters, reject_if: proc { |character| character[:name].blank? }
-
-  validates :name, length: {minimum: 2}
+  validates :name, length: { minimum: 2 }
   validates :user_id, presence: true
 
   validate :users_have_unique_shows
 
   def users_have_unique_shows
     errors.add(:show, "already exists; a user can't have two shows with the same name.") if self.user.shows.any? { |show| show.name == self.name }
+  end
+
+  def characters_attributes=(characters_attributes)
+    characters_attributes.each do |key, characters_attributes|
+      self.characters.build(characters_attributes)
+      self.characters.reject { |char| char.destroy if char.name.blank? }
+    end
   end
 
 end
