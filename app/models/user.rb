5a787ae39717523c3a667ee78efc4bcd46b84bda
DanL12186 <danlrcn@hotmail.com>
2017-05-25 13:41:47 -0400

    replaced old validation format with new one

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, allow_blank: true
   validates :password, length: { minimum: 4 }
-  validates_confirmation_of :password
+  validates :password, confirmation: true
   validates :name, length: {minimum: 3}, uniqueness: { case_sensitive: false }
 
   def self.find_or_create_by_omniauth(auth_hash)
