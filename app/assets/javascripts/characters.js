ef8b876079d4fb0ca8c4f1318c77af9e64066bdd
DanL12186 <danlrcn@hotmail.com>
2017-07-01 15:38:59 -0400

    Moved script to characters.js file in assets

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function () {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    const uid = $(".user").data("uid");
+    const sid = $("#character_show_id").val() //collection_select val
+    const serializedForm = $(this).serialize();
+    const characterCreation = $.post(`/users/${uid}/shows/${sid}/characters`, serializedForm);
+
+    characterCreation.done(function(char) { //data currently character object
+
+      const status = (`Status: ${char.deceased ? "Deceased" : "Alive"}`)
+      const hero = (char.dislike ? "Villain" : "Hero")
+      const newCharacter = `<h3> ${char.name} </h3>
+                            <div> ${char.note} </div>
+                            <div><em> ${char.quote} </em></div>
+                            <div> ${status} </div>
+                            <div> ${hero} </div>`
+
+      $("#newCharacters").append(newCharacter);
+
+      $('form').trigger('reset');
+    });
+  });
+});
