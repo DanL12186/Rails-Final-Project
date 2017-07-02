c74fc43b54b9dca769a0a923fecd8c6eaf095f20
DanL12186 <danlrcn@hotmail.com>
2017-07-01 21:52:04 -0400

    Added if statement to clumsily fix the issue of not being able to....
    …select the right submit button via a class or id when the js isn’t in
    a <script> tag on the same page as what it’s affecting.

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,40 +1,42 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function () { //ensure turbolinks without refresh
-  $('form').submit(function(event) {
-    event.preventDefault();
+  if ($("h1").html() === " New character ") { //clumbsy way of ensuring only the new character page (and not all forms) are used.
+    $("form").submit(function(event) {
+      event.preventDefault();
 
-    const uid = $(".user").data("uid");
-    const sid = $("#character_show_id").val() //collection_select val
-    const serializedForm = $(this).serialize();
-    const characterCreation = $.post(`/users/${uid}/shows/${sid}/characters`, serializedForm);
+      const uid = $(".user").data("uid");
+      const sid = $("#character_show_id").val() //collection_select val
+      const serializedForm = $(this).serialize();
+      const characterCreation = $.post(`/users/${uid}/shows/${sid}/characters`, serializedForm);
 
-    characterCreation.done(function(char) { //data currently character object
+      characterCreation.done(function(char) { //data currently character object
 
-      function Character(char) {
-      	this.name = char.name;
-      	this.deceased = char.deceased
-      	this.dislike = char.dislike
-      	this.quote = char.quote
-      	this.note = char.note
-      	this.id = char.id
-      	this.show = char.show
-        this.toDom = function() {
+        function Character(char) {
+        	this.name = char.name;
+        	this.deceased = char.deceased
+        	this.dislike = char.dislike
+        	this.quote = char.quote
+        	this.note = char.note
+        	this.id = char.id
+        	this.show = char.show
+          this.toDom = function() {
 
-          const status = (`Status: ${char.deceased ? "Deceased" : "Alive"}`)
-          const hero = (this.dislike ? "Villain" : "Hero")
-          const newCharacter = `<h3> ${this.name} </h3>
-                                <div> ${this.note} </div>
-                                <div><em> ${this.quote} </em></div>
-                                <div> ${status} </div>
-                                <div> ${hero} </div>`
+            const status = (`Status: ${char.deceased ? "Deceased" : "Alive"}`)
+            const hero = (this.dislike ? "Villain" : "Hero")
+            const newCharacter = `<h3> ${this.name} </h3>
+                                  <div> ${this.note} </div>
+                                  <div><em> ${this.quote} </em></div>
+                                  <div> ${status} </div>
+                                  <div> ${hero} </div>`
 
-          $("#newCharacters").append(newCharacter);
-          $('form').trigger('reset');
+            $("#newCharacters").append(newCharacter);
+            $('form').trigger('reset');
+          };
         };
-      };
-      const character = new Character(char);
-      character.toDom();
+        const character = new Character(char);
+        character.toDom();
+      });
     });
-  });
+  };
 });
