424d15224bf6de9d1c9c0c405de90cc418c5774d
DanL12186 <danlrcn@hotmail.com>
2017-09-10 18:44:48 -0400

    added back accepts_nested_attributes

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,21 +3,21 @@ class Show < ApplicationRecord
   belongs_to :user
   has_many :characters, dependent: :destroy
 
-  # accepts_nested_attributes_for :characters, reject_if: lambda { |character| character[:name].blank? }
+  accepts_nested_attributes_for :characters, reject_if: lambda { |character| character[:name].blank? }
 
   validates :name, length: { minimum: 2 }
   validates :user_id, presence: true
 
-  validate :users_have_unique_shows
+  validate :users_have_unique_shows, on: :create
 
   def users_have_unique_shows
     errors.add(:show, "already exists; a user can't have two shows with the same name.") if self.user.shows.any? { |show| show.name == self.name }
   end
 
-  def characters_attributes=(characters_attributes)
-    characters_attributes.each do |key, characters_attributes|
-      self.characters.build(characters_attributes)
-      self.characters.reject { |char| char.destroy if char.name.blank? }
-    end
-  end
+  # def characters_attributes=(characters_attributes)
+  #   characters_attributes.each do |key, characters_attributes|
+  #     self.characters.build(characters_attributes)
+  #     self.characters.reject { |char| char.destroy if char.name.blank? }
+  #   end
+  # end
 end
