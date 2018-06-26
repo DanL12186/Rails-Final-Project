7b3cb415eb04418e5382f6c1265d233c73f5fd71
DanL12186 <danlrcn@hotmail.com>
2018-06-26 12:05:24 -0400

    Cleaned up some helper code. Rewrote/fixed happiness calculator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,26 @@
 module ApplicationHelper
 
   def happiness
-    count = 0
     return 0 if self.characters.empty?
-    self.characters.each {|char| (char.dislike && !char.deceased || !char.dislike && char.deceased) ? count -= 0.5 : count += 0.5}
+    count = 0.0
+    unhappy_characters = self.characters.count { |char| !char.dislike && char.deceased || char.dislike && !char.deceased }
+    count += (self.characters.size - unhappy_characters)/2
+    count -= unhappy_characters/2
     return ((count / self.characters.size * 100) + 50).round(1)
   end
 
   def happiest
-    @shows.max_by { |show| show.happiness }
+    @shows.max_by(&:happiness)
   end
 
   def sort_by_name_length(show_list)
-    show_list.sort_by { |show| -show.name.size }
+    show_list.sort_by { |show| -show.name.length }
   end
 
   def number_to_word(number)
-    numbers = {  1=>"One", 2=>"Two", 3=>"Three", 4=>"Four", 5=>"Five",
-                 6=>"Six", 7=>"Seven", 8=>"Eight", 9=>"Nine"  }
-    numbers.detect { |num| num[0] == number }[1]
+    numbers = {  1=> "One", 2=> "Two", 3=> "Three", 4=> "Four", 5=> "Five",
+                 6=> "Six", 7=> "Seven", 8=> "Eight", 9=> "Nine"  }
+    numbers[number]
   end
 
   def icons
