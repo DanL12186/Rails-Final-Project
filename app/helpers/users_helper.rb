c1e2d73ba99f0cb312b117c413dd7c8cb231a1a8
DanL12186 <danlrcn@hotmail.com>
2019-02-13 19:44:05 -0500

    eager load chars in show w/most chars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
   def show_with_most_characters
-    @largest_show = current_user.shows.max_by { |show| show.characters.size }
+    @largest_show = current_user.shows.includes(:characters).max_by { |show| show.characters.size }
   end
 end
